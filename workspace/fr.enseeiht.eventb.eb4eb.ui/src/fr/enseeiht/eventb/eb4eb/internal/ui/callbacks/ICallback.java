a107ecc18fe0e10feb3a7893d34294415fa34705
SANGOI Remy <rsangoi@hp26-01.enseeiht.fr>
2026-03-20 16:01:25 +0100

    restructuration

--- workspace/fr.enseeiht.eventb.eb4eb.ui/src/fr/enseeiht/eventb/eb4eb/internal/ui/callbacks/ICallback.java
+++
@@ -1,5 +0,0 @@
-package fr.enseeiht.eventb.eb4eb.internal.ui.callbacks;
-
-public interface ICallback/*<O>*/ {
-//	public O call(Object... parameters);
-}
--- workspace/fr.enseeiht.eventb.eb4eb.ui/src/fr/enseeiht/eventb/eb4eb/internal/ui/visitor/IVisitor.java
+++
@@ -1,7 +0,0 @@
-package fr.enseeiht.eventb.eb4eb.internal.ui.visitor;
-
-import fr.enseeiht.eventb.eb4eb.internal.ui.builder.IBuilder;
-
-public interface IVisitor<Builder extends IBuilder> {
-
-}
